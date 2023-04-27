595d716c8b50d6ba2f19c4f86ff394f020ab1ace
Kolja Bruns <Kolja-bruns@gmx.de>
2023-04-27 21:15:38 +0200

    set cooldown UC test

--- app/src/main/kotlin/de/bitb/buttonbuddy/usecase/buddies/SetCooldownUC.kt
+++ app/src/main/kotlin/de/bitb/buttonbuddy/usecase/buddies/SetCooldownUC.kt
@@ -1,7 +1,10 @@
 package de.bitb.buttonbuddy.usecase.buddies
 
+import de.bitb.buttonbuddy.R
 import de.bitb.buttonbuddy.core.misc.Resource
+import de.bitb.buttonbuddy.core.misc.asResourceError
 import de.bitb.buttonbuddy.core.misc.calculateMilliseconds
+import de.bitb.buttonbuddy.core.misc.tryIt
 import de.bitb.buttonbuddy.data.BuddyRepository
 import de.bitb.buttonbuddy.data.UserRepository
 import de.bitb.buttonbuddy.data.model.Buddy
@@ -11,13 +14,18 @@ class SetCooldownUC(
     private val buddyRepo: BuddyRepository,
 ) {
     suspend operator fun invoke(buddy: Buddy, hour: Int, min: Int): Resource<Unit> {
-        val userResp = userRepo.getLocalUser()
-        if (userResp is Resource.Error) {
-            return userResp.castTo()
-        }
+        return tryIt {
+            val userResp = userRepo.getLocalUser()
+            if (userResp is Resource.Error) {
+                return@tryIt userResp.castTo()
+            }
+            if (!userResp.hasData) {
+                return@tryIt R.string.user_not_found.asResourceError()
+            }
 
-        val cooldown = calculateMilliseconds(hour, min)
-        val xBuddy = buddy.copy(cooldown = cooldown)
-        return buddyRepo.saveCooldown(userResp.data!!.uuid, xBuddy, cooldown)
+            val cooldown = calculateMilliseconds(hour, min)
+            val xBuddy = buddy.copy(cooldown = cooldown)
+            return@tryIt buddyRepo.saveCooldown(userResp.data!!.uuid, xBuddy, cooldown)
+        }
     }
 }
---
+++ app/src/test/kotlin/de/bitb/buttonbuddy/usecase/buddy/SetCooldownUCTest.kt
@@ -0,0 +1,127 @@
+package de.bitb.buttonbuddy.usecase.buddy
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import de.bitb.buttonbuddy.R
+import de.bitb.buttonbuddy.core.misc.Resource
+import de.bitb.buttonbuddy.data.UserRepository
+import de.bitb.buttonbuddy.data.model.User
+import de.bitb.buttonbuddy.shared.buildUser
+import de.bitb.buttonbuddy.core.getString
+import de.bitb.buttonbuddy.core.misc.DEFAULT_COOLDOWN
+import de.bitb.buttonbuddy.core.misc.asResourceError
+import de.bitb.buttonbuddy.core.misc.calculateMilliseconds
+import de.bitb.buttonbuddy.data.BuddyRepository
+import de.bitb.buttonbuddy.data.SettingsRepository
+import de.bitb.buttonbuddy.data.model.Buddy
+import de.bitb.buttonbuddy.shared.buildBuddy
+import de.bitb.buttonbuddy.ui.base.composable.ResString
+import de.bitb.buttonbuddy.ui.base.composable.asResString
+import de.bitb.buttonbuddy.usecase.buddies.ScanBuddyUC
+import de.bitb.buttonbuddy.usecase.buddies.SetCooldownUC
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SetCooldownUCTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    private val testDispatcher = StandardTestDispatcher()
+
+    private lateinit var mockUserRepo: UserRepository
+    private lateinit var mockBuddyRepo: BuddyRepository
+    private lateinit var setCooldownUC: SetCooldownUC
+
+    private val testHours: Int = 4
+    private val testMins: Int = 2
+
+    @After
+    fun cleanup() {
+        Dispatchers.resetMain()
+    }
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+        mockUserRepo = mockk()
+        mockBuddyRepo = mockk()
+        setCooldownUC = SetCooldownUC(mockUserRepo, mockBuddyRepo)
+    }
+
+    //       verify { match {
+//            it.asString(::getString) == ResString.ResourceString(
+//                R.string.message_sent_toast,
+//                arrayOf(buddy.fullName),
+//            ).asString(::getString)
+//        }}
+    @Test
+    fun `get user error, should return error`() = runTest {
+        val buddy = buildBuddy()
+        val expectedError = "Database Error".asResourceError<User?>()
+        coEvery { mockUserRepo.getLocalUser() } returns expectedError
+
+        val errorResp = setCooldownUC(buddy, testHours, testMins)
+        assert(errorResp is Resource.Error)
+        assertEquals(
+            errorResp.message!!.asString(::getString),
+            expectedError.message!!.asString(::getString)
+        )
+    }
+
+    @Test
+    fun `No user found, should return error`() = runTest {
+        val buddy = buildBuddy()
+        coEvery { mockUserRepo.getLocalUser() } returns Resource.Success(null)
+
+        val errorResp = setCooldownUC(buddy, testHours, testMins)
+        assert(errorResp is Resource.Error)
+        assertEquals(
+            errorResp.message!!.asString(::getString),
+            R.string.user_not_found.asResourceError<User>().message!!.asString(::getString)
+        )
+    }
+
+    @Test
+    fun `save cooldown failed, should return error`() = runTest {
+        val buddy = buildBuddy()
+        val expectedError = "Save Cooldown Error".asResourceError<Unit>()
+        coEvery { mockUserRepo.getLocalUser() } returns Resource.Success(buildUser())
+        coEvery { mockBuddyRepo.saveCooldown(any(), any(), any()) } returns expectedError
+
+        val errorResp = setCooldownUC(buddy, testHours, testMins)
+        assert(errorResp is Resource.Error)
+        assertEquals(
+            errorResp.message!!.asString(::getString),
+            expectedError.message!!.asString(::getString)
+        )
+    }
+
+    @Test
+    fun `cooldown saved, should return success`() = runTest {
+        val buddy = buildBuddy()
+        val resultCooldown = calculateMilliseconds(testHours, testMins)
+        coEvery { mockUserRepo.getLocalUser() } returns Resource.Success(buildUser())
+        coEvery { mockBuddyRepo.saveCooldown(any(), any(), any()) } returns Resource.Success()
+
+        assert(buddy.cooldown == DEFAULT_COOLDOWN)
+        val successResp = setCooldownUC(buddy, testHours, testMins)
+        assert(successResp is Resource.Success)
+        coVerify {
+            mockBuddyRepo.saveCooldown(
+                any(),
+                match { it.cooldown == resultCooldown },
+                match { it == resultCooldown },
+            )
+        }
+    }
+}
